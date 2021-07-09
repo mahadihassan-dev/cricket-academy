8270258caf467ad4b7abce6863ef1f8fe29a802d
Mahedi Hassan <mahadihassan.mhd@gmail.com>
2021-07-09 12:44:47 +0600

    font-awesome Check

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,45 @@
 // Jquery Plugin Initializing
 $(document).ready(function(){
+    // Banner Slider
+    $('.banner_slider').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        autoplay: true,
+        arrows: true,
+        nextArrow: '<i class="fas fa-arrow-right"></i>',
+        prevArrow: '<i class="fas fa-arrow-left"></i>',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
     // sticky menu 
     var navoff = $(".header_navbar").offset().top;
 
@@ -12,4 +52,14 @@ $(document).ready(function(){
             $(".header_navbar").removeClass("sticky_menu");
         }
     });
+     // Our Practice Session Video Gallery
+     $('.gallery_video_frame').venobox({
+        framewidth : '400px',                           
+        frameheight: '300px',                           
+        border     : '10px',                            
+        bgcolor    : '#5dff5e',                    
+        numeratio  : false,                              
+        infinigall : false,                              
+        share      : ['facebook', 'twitter', 'download'],
+      }); 
 })
